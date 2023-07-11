21153e2bbc8fea960c7a155eb4be3faef9111890
danielpqb <danielpqb@gmail.com>
2023-07-11 19:12:25 -0300

    Database URL updated

--- src/database/database.js
+++ src/database/database.js
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
 
-const mongoClient = new MongoClient(process.env.DB_URI);
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
 try {
   await mongoClient.connect();
   console.log("Connected to MongoDB");
