36d5a26847bcdb77656152705478038bdfe08d2a
danielpqb <danielpqb@gmail.com>
2023-07-11 18:38:08 -0300

    Fix router

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(userRouter);
+app.use("/user", userRouter);
 
 app.listen(process.env.PORT, () => {
   console.log("Server running on port " + process.env.PORT);
