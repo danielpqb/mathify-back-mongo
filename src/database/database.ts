a3810480f145d792b97874885f9ded65267bdb6f
danielpqb <danielpqb@gmail.com>
2023-07-20 10:30:21 -0300

    husky and lintstaged

--- src/database/database.ts
+++ src/database/database.ts
@@ -8,8 +8,7 @@ async function connectToDB() {
   try {
     await mongoClient.connect();
     console.log("Connected to MongoDB");
-  }
-  catch (error) {
+  } catch (error) {
     console.log(error);
     console.log("Connection to MongoDB failed!");
   }
