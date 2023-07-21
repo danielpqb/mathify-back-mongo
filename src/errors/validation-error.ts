da57b51e3f33a07bd364ef81af4d6cf816ad0e72
danielpqb <danielpqb@gmail.com>
2023-07-20 22:44:12 -0300

    Joining message correctly

--- src/errors/validation-error.ts
+++ src/errors/validation-error.ts
@@ -2,13 +2,13 @@ import { ValidationError } from "joi";
 import { ApplicationError } from "../types/Error";
 
 export function validationError(validation: ValidationError): ApplicationError {
-  let message = "";
+  const message: string[] = [];
   validation.details.forEach((e) => {
-    message += e.message + "\n\n";
+    message.push(e.message);
   });
 
   return {
     name: "ValidationError",
-    message: message,
+    message: message.join("\n\n"),
   };
 }
